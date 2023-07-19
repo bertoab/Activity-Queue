744f0fd42aae2b381056420168f6fe198dc8cc46
Roberto Baez <r.baez3672@gmail.com>
2023-07-19 15:25:32 -0400

    test (script.test.js): restructure test file for MVVM architecture
     import IIFEs into global variables
     remove "describe" calls used for previous program architecture
     add "describe" calls to outline basic functionality of Model, ViewModel, and View

--- script.test.js
+++ script.test.js
@@ -2,7 +2,8 @@
  * @jest-environment jsdom
  */
 
-let script; // imports to be defined here
+let script;
+let model, viewModel, view;
 
 beforeAll(() => {
 	// set up DOM
@@ -16,47 +17,18 @@ beforeAll(() => {
   </div>
   `;
 	
-	// load module
-	script = require('./script'); // side-effect (DOM modification)
+  // load module
+  script = require('./script');
+  model = script.Model;
+  viewModel = script.ViewModel(model);
+  view = script.View(viewModel); // side-effect (DOM modification)
 });
 
-describe('contentTable', () => {
-  describe('input validation', () => {
-    test('throw error when called with no arguments', () => {
-      expect(() => script.contentTable()).toThrow(TypeError);
-    });
-    test('throw error when called with only one argument', () => {
-      expect(() => script.contentTable(null)).toThrow(TypeError);
-      expect(() => script.contentTable(423)).toThrow(TypeError);
-      expect(() => script.contentTable("any")).toThrow(TypeError);
-      expect(() => script.contentTable(["any"])).toThrow(TypeError);
-      expect(() => script.contentTable({})).toThrow(TypeError);
-    });
-    test('throw error when called with two non-array arguments', () => {
-      expect(() => script.contentTable(NaN, null)).toThrow(TypeError);
-      expect(() => script.contentTable("any", "one")).toThrow(TypeError);
-      expect(() => script.contentTable(243, {})).toThrow(TypeError);
-      expect(() => script.contentTable({}, 243)).toThrow(TypeError);
-    });
-    test('throw error when called with one array and one non-array argument', () => {
-      expect(() => script.contentTable(["any", "two"], "one")).toThrow(TypeError);
-      expect(() => script.contentTable("one", ["any"])).toThrow(TypeError);
-    });
+describe('Model', () => {
+  describe('data management', () => {
   });
 });
-
-describe('FunctionBar', () => {
-  describe('input validation', () => {
-    test('throw error when called with no arguments', () => {
-      expect(() => script.FunctionBar()).toThrow(TypeError);
-    });
-    test('throw error when called with non-object first and/or second argument', () => {
-      expect(() => script.FunctionBar({}, [])).toThrow(TypeError);
-      expect(() => script.FunctionBar([], {})).toThrow(TypeError);
-      expect(() => script.FunctionBar("", "")).toThrow(TypeError);
-      expect(() => script.FunctionBar("")).toThrow(TypeError);
-      expect(() => script.FunctionBar(43242)).toThrow(TypeError);
-      expect(() => script.FunctionBar(null)).toThrow(TypeError);
-    });
-  });
+describe('ViewModel', () => {
+  describe('validateUserFunction', () => {});
 });
+describe('View', () => {});
