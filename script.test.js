a0d6992974f8387ffc8fbec22f28b3b29a8a999d
Roberto Baez <r.baez3672@gmail.com>
2023-07-23 09:46:49 -0400

    test (script.test.js): implement basic outline of ViewModel's "validateUserFunction" tests
    implement "createPassFailFunctions" global function with docstring
    implement "createSyntheticKeyboardEvent" function with docstring
    add descriptive set of "test.todo" calls for testing "validateUserFunction"

--- script.test.js
+++ script.test.js
@@ -4,6 +4,11 @@
 
 let script;
 let model, viewModel, view;
+/**
+ * Create a pair of "pass" and "fail" jest mock functions to track callback executions within tests
+ * @returns {[jest.fn, jest.fn]}
+ */
+const createPassFailFunctions = () => [jest.fn(), jest.fn()];
 
 beforeAll(() => {
 	// set up DOM
@@ -29,6 +34,23 @@ describe('Model', () => {
   });
 });
 describe('ViewModel', () => {
-  describe('validateUserFunction', () => {});
+  describe('validateUserFunction', () => {
+    /**
+     * Create an Event-like object used to invoke ViewModel's "validateUserFunction"
+     * @param {string} startingInput
+     */
+    const createSyntheticKeyboardEvent = function(startingInput) {
+      return {
+        key: "Enter",
+        target: { value: startingInput }
+      }
+    };
+
+    test.todo("when multiple user function acronym strings are present, only match the first one");
+    test.todo("match user function acronym strings in a case-insensitive manner");
+    test.todo("when user function acronym strings are 2+ letters in length, match them before those of lesser length");
+    test.todo("when a user function acronym string and a single integer index content item are matched, user function is called with corresponding content item value")
+    test.todo("when a user function acronym string and two integer index content items are matched, user function is called with first matched content item value as first argument, and second matched content item value as second argument")
+  });
 });
 describe('View', () => {});
