87e9cc587d47618a28f23cb8d6bffd683111c14d
Roberto Baez <r.baez3672@gmail.com>
2023-07-25 23:09:08 -0400

    test (script.test.js): implement test for "validateUserFunction"
    test descriptor is "when user function acronym strings are 2+ letters in length, match them before those of lesser length"

--- script.test.js
+++ script.test.js
@@ -111,7 +111,46 @@ describe('ViewModel', () => {
       expect(pass.mock.calls).toHaveLength(5);
       expect(fail.mock.calls).toHaveLength(1);
     });
-    test.todo("when user function acronym strings are 2+ letters in length, match them before those of lesser length");
+    test("when user function acronym strings are 2+ letters in length, match them before those of lesser length", () => {
+      // run 1 (setup)
+      const [pass, fail] = createPassFailFunctions();
+
+      viewModel.state.functionMapping = {
+        "A": fail,
+        "ABC": pass,
+        "BC": pass,
+        "GH": fail,
+        "GHIJ": pass,
+        "DEF": pass
+      };
+
+      const event = createSyntheticKeyboardEvent("ABC"); // PASS
+      viewModel.validateUserFunction(event);
+
+      // run 2
+      event.target.value = "LMOBCA"; // PASS
+      viewModel.validateUserFunction(event);
+
+      // run 3
+      event.target.value = "GHIJK"; // PASS
+      viewModel.validateUserFunction(event);
+
+      // run 4
+      event.target.value = "GBDEF"; // PASS
+      viewModel.validateUserFunction(event);
+
+      // run 5
+      event.target.value = "AHBC"; // FAIL
+      viewModel.validateUserFunction(event);
+      
+      // run 6
+      event.target.value = "GHABC"; // FAIL
+      viewModel.validateUserFunction(event);
+
+      // assertions
+      expect(pass.mock.calls).toHaveLength(4);
+      expect(fail.mock.calls).toHaveLength(2);
+    });
     test.todo("when a user function acronym string and a single integer index content item are matched, user function is called with corresponding content item value")
     test.todo("when a user function acronym string and two integer index content items (separated by a comma) are matched, user function is called with first matched content item value as first argument, and second matched content item value as second argument")
   });
